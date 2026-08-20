8a68690fa2d426adbe6caf10c8447e9375bcbf69
Esther <168462264+Estherin9@users.noreply.github.com>
2026-08-20 14:33:44 +0100

    blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
